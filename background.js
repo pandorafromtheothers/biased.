429abe9f2266c26a4429af881f94eaf7c33a683b
pandorafromtheothers <115832798+pandorafromtheothers@users.noreply.github.com>
2025-03-10 20:11:25 +0100

    easier to understand scripting, no reload needed for tracklist

--- background.js
+++ background.js
@@ -1,21 +1,13 @@
 chrome.action.onClicked.addListener((tab) => {
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: async () => {
-            return await moshimoshi().then((result) => {
-                return result;
-            });
-        }
-    }).then((response) => {
+    chrome.scripting.executeScript({target: { tabId: tab.id },func: () => getList()}).then((response) => {
         let _result = response[0].result;
         if (_result == null)
             return;
         chrome.tabs.create({ url: chrome.runtime.getURL("index.html") }, (newTab) =>
-            chrome.tabs.onUpdated.addListener(function listener(tabId, info) {
-                if (tabId === newTab.id && info.status === 'complete') {
-                    chrome.tabs.onUpdated.removeListener(listener);
-                    chrome.tabs.sendMessage(newTab.id, { message: _result });
-                }
-            }))
-    })
-})
+        chrome.tabs.onUpdated.addListener(function listener(tabId, info) {
+            if (tabId == newTab.id && info.status == 'complete') {
+                chrome.tabs.onUpdated.removeListener(listener);
+                chrome.tabs.sendMessage(newTab.id, { message: _result });
+            }
+        }))
+})});
--- main.js
+++ main.js
@@ -1,29 +1,16 @@
-let Global = undefined;
 function getList() {
     let _result = {} || undefined;
-    let _htmlList = document.getElementsByClassName("title style-scope ytmusic-responsive-list-item-renderer complex-string");
+
+    let _htmlList = document.querySelectorAll("ytmusic-responsive-list-item-renderer[page-type]");
     _result.artist = document.getElementsByClassName("strapline-text style-scope ytmusic-responsive-header-renderer complex-string")[0].title;
     _result.album = document.getElementsByClassName("title style-scope ytmusic-responsive-header-renderer")[0].title;
 
     let _tracks = [];
     for (let index = 0; index < _htmlList.length; index++) {
-        _tracks.push(_htmlList[index].textContent);
+        _tracks.push(_htmlList[index].querySelector("a").textContent);
     }
     _result.tracks = _tracks;
 
     console.log(_result);
     return _result;
-}
-
-addEventListener("message", (event) => {
-    if (event.data != "GETLIST")
-        return;
-
-    Global = getList();
-});
-function moshimoshi() {
-    window.postMessage("GETLIST");
-    return new Promise((resolve)=> {
-        setTimeout(() => { resolve(Global); }, 500);
-    });
 }
